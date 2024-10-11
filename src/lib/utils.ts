352f0dc24ccce43ab6a5a8935b05e932917d5693
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2024-10-12 01:51:02 +0600

    test completed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
+const API_URL = process.env.API_URL || 'http://localhost:3000';
+
 export const data = async (endpoint: string) => {
   try {
-    const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + endpoint);
+    const res = await fetch(API_URL + endpoint);
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
