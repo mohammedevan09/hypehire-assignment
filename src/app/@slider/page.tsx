fdc660bdb1d3bca062f72959c9e5ad4de4f0786e
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2024-10-12 01:54:37 +0600

    test completed

--- src/app/@slider/page.tsx
+++ src/app/@slider/page.tsx
@@ -3,6 +3,8 @@ import { SliderData } from '@/lib/types';
 import { data } from '@/lib/utils';
 import React from 'react';
 
+export const dynamic = 'force-dynamic';
+
 const page = async () => {
   const sliderData: SliderData[] = await data('/api/sliderData');
   return <Slider sliderData={sliderData} />;
